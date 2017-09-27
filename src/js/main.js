a374aa6b323162f0614b6c100f65daad7e682a4c
Alice Kasyanova <liskas87@mail.ru>
2017-09-27 14:36:11 +0300

    Block-price. Реализовано hover состояние. Добавлены иконки в block-feature. Добавлены кнопки навигации в слайдер block-about. Внесены изменения в html

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,6 @@
 $( document ).ready(function() {
 
-/////////////////////////////////////////////////////	// 
-	 $('.slaider').slick({
+	 $('.about-slider').slick({
                     speed: 7000,
                     autoplay: true,
                     arrows: true,
@@ -9,67 +8,14 @@ $( document ).ready(function() {
                     nextArrow:    '<img class="controls left-control slick-prev" src="img/slaid-left.png">',
 
                 });
-// ///////////////////////////////////////////////////
-	var latitude = 30.258940,
-	    longitude = -81.392700,
-	    map_zoom = 16;
+// ////////////////////////////
 
-	var marker_img = 'img/marker.png';
-	var main_color = '#fef7';
-
-	var style = [
-  {
-    "stylers": [
-  { "saturation": -100 }
-]
-  }
-];
-	var map_options = {
-		center: new google.maps.LatLng(latitude, longitude),
-		zoom: map_zoom,
-		panControl: false,
-		zoomControl: true,
-		mapTypeControl: false,
-		streetViewControl: false,
-		mapTypeId: google.maps.MapTypeId.ROADMAP,
-		scrollwheel: false,
-		styles: style
-	}  
-	//инициализация карты
-
-	var map = new google.maps.Map(document.getElementById('google-map'), map_options);
-	//cвой маркер
-
-	var marker = new google.maps.Marker({
-		position: new google.maps.LatLng(latitude, longitude),
-		map: map,
-		visible: true,
-		zIndex: 999,
-		icon: marker_img
-	});
-// /////////////////////////////////////////////////////////////////////
-
-
-     var wow = new WOW(
-        {
-        boxClass:     'wow',      
-        animateClass: 'animated', 
-        offset:       0,         
-        mobile:       true,       
-        live:         true,       
-        callback:     function(box) {
-          },
-        scrollContainer: null 
-              }
-            );
-            wow.init();
-            
-       
+ $('.btn').hover(function() {
+$(this).closest('.price').find('.price-title').toggleClass('hover');
 });
 
-// /////////////////////////////////////////////////////////////////////
+  });
 
 
-
-
+	
     
